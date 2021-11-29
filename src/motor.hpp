e60c78ebe24ae671ad69b4ace5e07f45bcd4318f
phil <me@filou.se>
2021-11-29 16:23:51 +0100

    removed header

--- src/motor.hpp
+++ src/motor.hpp
@@ -1,20 +1,3 @@
-/**
- * AES67 Framework
- * Copyright (C) 2021  Philip Tschiemer, https://github.com/tschiemer/aes67
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 #ifndef MOBSPKR_VEHICLE_CTRL_MOTOR_HPP
 #define MOBSPKR_VEHICLE_CTRL_MOTOR_HPP
@@ -216,7 +199,7 @@ class Motor {
                 uint8_t command_number() const { return m_bytes[3]; }
                 uint32_t value() const { return (m_bytes[4] << 24) | (m_bytes[5] << 16) | (m_bytes[6] << 8) | m_bytes[7]; }
                 uint8_t checksum() const { return m_bytes[8]; }
-            
+
                 bool valid() const { return m_bytes[8] == m_checksum; }
 
                 void set(uint8_t response[9]){
